6b7949b7c590d88574ff4620501a6b8cc1b4a744
wytprogramer <1124483169@qq.com>
2021-03-22 23:37:11 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import iView from 'iview'
+// import 'iview/dist/styles/iview.css'
+// Vue.use(iView)
+import './common/iconfont.js'
+
+import VideoPlayer from 'vue-video-player'
+import 'vue-video-player/src/custom-theme.css'
+import 'video.js/dist/video-js.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+Vue.use(VideoPlayer)
+
 Vue.config.productionTip = false
 
 new Vue({
